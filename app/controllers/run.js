fc5538ad599b796f9eac5b7b5e398d77e156cf10
Jiangning Huang <hjnyll@163.com>
2015-12-11 21:45:48 +0800

    jiaodarun4

--- app/controllers/run.js
+++ app/controllers/run.js
@@ -13,9 +13,23 @@ var getConnection = function() {
 // index page
 exports.result = function(req, res) {
   var name = req.body.name;
-  console.log(name);
+  // console.log(name);
   var conn = getConnection();
-  conn.query('SELECT * FROM profiles where name="' + name + '"', function (error, results, fields) {
+  if (!name || name.indexOf('delete') >= 0
+        || name.indexOf('drop') >= 0
+        || name.indexOf('*') >= 0
+        || name.indexOf('true') >= 0
+        || name.indexOf('select') >= 0
+        || name.indexOf('1') >= 0
+        || name.indexOf('where') >= 0) {
+    res.render('error', {
+      title: '交大 uRun',
+      tip: '信息有误, 请重新查询'
+    });
+    return;
+  }
+  else {
+    conn.query('SELECT * FROM profiles where name="' + name + '" or partnername ="' + name + '"', function (error, results, fields) {
         if (error) {
           res.render('error', {
             title: '交大 uRun',
@@ -23,11 +37,10 @@ exports.result = function(req, res) {
             error: error
           });
           return;
-        }
-        if(!results || results.length === 0) {
+        } else if(!results || results.length === 0) {
           res.render('error', {
             title: '交大 uRun',
-            tip: '没有查到相关信息，请返回重新查询',
+            tip: '没有找到您的报名信息，请返回重新查询',
             error: error
           });
           return;
@@ -40,6 +53,7 @@ exports.result = function(req, res) {
           return;
         }
     });
+  }
 };
 exports.query = function(req, res) {
   res.render('query', {
