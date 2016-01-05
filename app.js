f76f2658e6dd7927d83983b15050dd27bd5b5432
Jiangning Huang <hjnyll@163.com>
2016-01-05 21:37:07 +0800

    zuimeng

--- app.js
+++ app.js
@@ -57,7 +57,41 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 
 app.use('/weixin', wechat(config.weixin, function (req, res, next) {
-    res.send('true');
+    // 微信输入信息都在req.weixin上
+    var message = req.weixin;
+    console.log(message);
+    if (message.FromUserName === 'diaosi') {
+    // 回复屌丝(普通回复)
+        res.reply('hehe');
+    } else if (message.FromUserName === 'text') {
+        //你也可以这样回复text类型的信息
+        res.reply({
+          content: 'text object',
+          type: 'text'
+        });
+    } else if (message.FromUserName === 'hehe') {
+        // 回复一段音乐
+        res.reply({
+          type: "music",
+          content: {
+            title: "来段音乐吧",
+            description: "一无所有",
+            musicUrl: "http://mp3.com/xx.mp3",
+            hqMusicUrl: "http://mp3.com/xx.mp3",
+            thumbMediaId: "thisThumbMediaId"
+          }
+        });
+    } else {
+        // 回复高富帅(图文回复)
+        res.reply([
+          {
+            title: '你来我家接我吧',
+            description: '这是女神与高富帅之间的对话',
+            picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+            url: 'http://nodeapi.cloudfoundry.com/'
+          }
+        ]);
+    }
 }));
 
 require('./config/routes')(app);
